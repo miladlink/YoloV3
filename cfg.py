46caa25c908a0336ebd396c8bf3533a652dd5da6
milad.hasani <milad.link.007@gmail.com>
2022-01-11 21:46:28 +0330

    rename init & modify cfg

---
+++ cfg.py
@@ -0,0 +1,7 @@
+import torch
+from model import load_model
+
+DEVICE = torch.device ('cuda' if torch.cuda.is_available () else 'cpu')
+MODEL = load_model ('weights/yolov3.weights', DEVICE)
+with open ('class_names', 'r') as f:
+    CLASS_NAMES = f.read ().split ('\n')
